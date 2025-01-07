e72c6c96e586097a9208b68539673d287b54293e
RichelleReyes <richelle.reyes@uabc.edu.mx>
2025-01-07 14:06:36 -0800

    receipt update

--- src/helpers/PrintAction.tsx
+++ src/helpers/PrintAction.tsx
@@ -20,10 +20,10 @@ const PrintAction = () => {
     try {
       const htmlResponse = await fetch("/utils/receipt-files/a-formato/receipt.html");
       let html = await htmlResponse.text();
-
-      const cssResponse = await fetch("/utils/receipt-files/a-formato/css/style.css");
-      const css = await cssResponse.text();
-      html = html.replace("</head>", `<style>${css}</style></head>`);
+      //
+      // const cssResponse = await fetch("/utils/receipt-files/a-formato/css/style.css");
+      // const css = await cssResponse.text();
+      // html = html.replace("</head>", `<style>${css}</style></head>`);
 
       // const jsResponse = await fetch("/utils/receipt-files/receipt-default/js/script.js");
       // let js = await jsResponse.text();
